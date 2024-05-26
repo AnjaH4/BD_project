eb451e0681eb05c7d37180f2cd8979aad723c019
AnjaH4 <ah40785@student.uni-lj.si>
2024-05-26 21:14:38 +0200

    popravljeni utilsi

--- utils.py
+++ utils.py
@@ -1,16 +1,34 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-import vaex
+#import vaex
 
-def transform_parquet(file_path): #file_path = path to the csv file
+# def transform_parquet(file_path): #file_path = path to the csv file
+#     parquet_file = file_path.replace('.csv', '.parquet')
+#     chunksize = 100_000
+
+#     csv_stream = pd.read_csv(file_path, sep='\t', chunksize=chunksize, low_memory=False)
+
+#     for i, chunk in enumerate(csv_stream):
+#         #print("Chunk", i)
+#         if i == 0:
+#             # Guess the schema of the CSV file from the first chunk
+#             parquet_schema = pa.Table.from_pandas(df=chunk).schema
+#             # Open a Parquet file for writing
+#             parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
+#         # Write CSV chunk to the parquet file
+#         table = pa.Table.from_pandas(chunk, schema=parquet_schema)
+#         parquet_writer.write_table(table)
+
+#     parquet_writer.close()
+
+def transform_parquet(file_path):  # file_path = path to the csv file
     parquet_file = file_path.replace('.csv', '.parquet')
     chunksize = 100_000
 
-    csv_stream = pd.read_csv(file_path, sep='\t', chunksize=chunksize, low_memory=False)
+    csv_stream = pd.read_csv(file_path, sep='\t', chunksize=chunksize, low_memory=False, on_bad_lines='skip')
 
     for i, chunk in enumerate(csv_stream):
-        print("Chunk", i)
         if i == 0:
             # Guess the schema of the CSV file from the first chunk
             parquet_schema = pa.Table.from_pandas(df=chunk).schema
@@ -21,6 +39,7 @@ def transform_parquet(file_path): #file_path = path to the csv file
         parquet_writer.write_table(table)
 
     parquet_writer.close()
+
 
 
 # def transform_hdf5(file_path):
