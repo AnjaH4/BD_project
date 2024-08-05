b463fd83d1c11eae9d49f852e743fd83518af294
AnjaH4 <ah40785@student.uni-lj.si>
2024-08-05 12:13:12 +0200

    low memory false when reading the data

--- transform_to_hdf5.py
+++ transform_to_hdf5.py
@@ -7,7 +7,7 @@ filepaths = glob.glob('/d/hpc/projects/FRI/bigdata/students/mk75264/data/augment
 for filepath in filepaths:
     # Read the augmented data
     print(f"Reading {filepath}")
-    augmented_data = pd.read_csv(filepath)
+    augmented_data = pd.read_csv(filepath, low_memory=False)
 
     # Save the augmented data to a HDF5 file
     augmented_data.to_hdf(filepath.replace('.csv', '.h5'), key='data', mode='w')
