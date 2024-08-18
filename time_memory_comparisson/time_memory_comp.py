8bdeedd6a3810ea5f8c6c60d75e92c24ac98515d
AnjaH4 <ah40785@student.uni-lj.si>
2024-08-18 19:46:26 +0200

    nastavitve dask clustra za timing

--- time_memory_comparisson/time_memory_comp.py
+++ time_memory_comparisson/time_memory_comp.py
@@ -9,7 +9,7 @@ def main(args):
     cluster = LocalCluster(n_workers=10,  threads_per_worker=8, memory_limit="6GB")
     client = Client(cluster)
 
-    df = dd.read_parquet(os.path.join(args.path, "*.parquet")).sample(frac=1, random_state=42)
+    df = dd.read_parquet(os.path.join(args.path, "*.parquet")).sample(frac=0.1, random_state=42)
 
     #time to read the data
     start = datetime.now()
